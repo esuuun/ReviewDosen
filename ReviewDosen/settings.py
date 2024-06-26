647aaff7180a2d4aeb7699c23241dc98a33f59d0
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-06-26 21:01:43 +0700

    staticfiles dirs

--- ReviewDosen/settings.py
+++ ReviewDosen/settings.py
@@ -187,10 +187,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STORAGES = {
