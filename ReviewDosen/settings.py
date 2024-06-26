664029f319473300a12a96a1eefd0dd669bf8a9d
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-06-26 19:48:29 +0700

    whitenoise storage

--- ReviewDosen/settings.py
+++ ReviewDosen/settings.py
@@ -193,12 +193,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STORAGES = {
-
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
