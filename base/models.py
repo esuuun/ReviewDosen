c62bf2af5f4d8cc8b81905170f51e145715cdb6f
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-06-26 17:39:44 +0700

    updated model

--- base/models.py
+++ base/models.py
@@ -1,8 +1,6 @@
-from django.db import models
+from django.db import connection, models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-# Create your models here.
 
 class Fakultas(models.Model):
     name = models.CharField(max_length=200)
@@ -19,7 +17,7 @@ class DosenGrade(models.Model):
 
 class Dosen(models.Model):
     name = models.CharField(max_length=200)
-    fakultas = models.ForeignKey(Fakultas,on_delete=models.SET_NULL, null=True)
+    fakultas = models.ForeignKey(Fakultas, on_delete=models.SET_NULL, null=True)
     matakuliah = models.CharField(max_length=100)
     rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
     updated = models.DateTimeField(auto_now=True)
@@ -31,15 +29,26 @@ class Dosen(models.Model):
     def __str__(self):
         return self.name
 
-class Review (models.Model):
+class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.FloatField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     dosen = models.ForeignKey(Dosen, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
-    grade = models.ForeignKey(DosenGrade, on_delete=models.CASCADE,null=False)
+    grade = models.ForeignKey(DosenGrade, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.body[0:50]
 
+    def clean_body(self):
+        with connection.cursor() as cursor:
+            query = """
+                SELECT (regexp_replace(%s::text, '\\u0000', '', 'g'))::text;
+            """
+            cursor.execute(query, [self.body])
+            result = cursor.fetchone()
+            self.body = result[0] if result else self.body
 
+    def save(self, *args, **kwargs):
+        self.clean_body()
+        super(Review, self).save(*args, **kwargs)
