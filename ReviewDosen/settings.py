5a41d6b5b2e0371ccc3fc5149dae073fca6eeaa5
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-06-26 19:54:43 +0700

    more

--- ReviewDosen/settings.py
+++ ReviewDosen/settings.py
@@ -187,7 +187,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme')]
