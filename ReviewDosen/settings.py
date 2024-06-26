8a937d48b7e3619c6fa09b93a3836068e1346875
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-06-26 16:05:21 +0700

    i hope it work

--- ReviewDosen/settings.py
+++ ReviewDosen/settings.py
@@ -142,10 +142,10 @@ DATABASES = {
     }
 }
 
-POSTGRES_LOCALLY = False
+# POSTGRES_LOCALLY = False
 
-if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
-    DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
+# if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
+#     DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
 
 # Password validation
