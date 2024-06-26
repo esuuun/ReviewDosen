febc51f96dde014698cca12df158cab24e437fe2
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-06-26 17:47:04 +0700

    dfasfdsa

--- ReviewDosen/settings.py
+++ ReviewDosen/settings.py
@@ -139,9 +139,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'OPTIONS': {
-            'client_encoding': 'UTF8',
-        },
     }
 }
 
