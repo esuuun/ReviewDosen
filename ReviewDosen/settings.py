ad8e1a6a37a7846957cf498c2f6ab4c9b596d9e0
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-06-26 17:01:58 +0700

    client encoding UTF8

--- ReviewDosen/settings.py
+++ ReviewDosen/settings.py
@@ -139,6 +139,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        },
     }
 }
 
