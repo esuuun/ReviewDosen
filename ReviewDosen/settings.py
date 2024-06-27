c6d303ac1cadb7b1fc6825fb7a0614f1cc500a9c
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-06-27 09:44:36 +0700

    pls

--- ReviewDosen/settings.py
+++ ReviewDosen/settings.py
@@ -191,7 +191,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STORAGES = {
