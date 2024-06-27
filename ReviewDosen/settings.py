57b1bcbefefb10b39c18c5ec9263eca2ab9a36a3
Muhammad Ikhsan Kurniawan <ragendoft05@gmail.com>
2024-06-27 08:50:19 +0700

    static

--- ReviewDosen/settings.py
+++ ReviewDosen/settings.py
@@ -19,7 +19,7 @@ Env.read_env()
 ENVIRONMENT = env('ENVIRONMENT', default='production')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
